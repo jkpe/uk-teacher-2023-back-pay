5611b9ee48e80594ca9258d919a324c593350f8f
jkpe <16779171+jkpe@users.noreply.github.com>
2023-12-02 20:05:17 +0000

    Refactor pay calculation and add tax calculation

--- script.js
+++ script.js
@@ -2,15 +2,41 @@
 document.getElementById("payCalculator").addEventListener("submit", function(event){
     event.preventDefault();
 
-    // Get the current salary and student loan plan
-    const currentSalary = document.getElementById("currentSalary").value;
-    const studentLoanPlan = document.getElementById("studentLoan").value;
+    // Get the current salary
+    const currentSalary = parseFloat(document.getElementById("currentSalary").value);
+    const monthlySalary = currentSalary / 12;
 
-    // Calculate the new monthly salary
-    const newMonthlySalary = (currentSalary * 1.065) / 12;
-    const oldMonthlySalary = currentSalary / 12;
-    const backDatedPay = (newMonthlySalary * 2) - (oldMonthlySalary * 2);
+    // Calculate the new monthly salary after the pay rise
+    const newMonthlySalary = monthlySalary * 1.065;
+
+    // Calculate back-dated pay (2 months)
+    let backDatedPay = (newMonthlySalary * 2) - (monthlySalary * 2);
+
+    // Calculate the tax for the back-dated pay
+    let tax = calculateTax(backDatedPay);
+
+    // Subtract tax from the back-dated pay to get the net total
+    let netBackDatedPay = backDatedPay - tax;
 
     // Display the result
-    document.getElementById("result").innerHTML = `Your back-dated pay is: £${backDatedPay.toFixed(2)}`;
+    document.getElementById("result").innerHTML = `Your net back-dated pay is: £${netBackDatedPay.toFixed(2)}`;
 });
+
+function calculateTax(income) {
+    const personalAllowance = 12570;
+    const basicRateUpperLimit = 50270;
+    const higherRateUpperLimit = 150000;
+    let tax = 0;
+
+    if (income <= personalAllowance) {
+        tax = 0;
+    } else if (income <= basicRateUpperLimit) {
+        tax = (income - personalAllowance) * 0.20;
+    } else if (income <= higherRateUpperLimit) {
+        tax = (basicRateUpperLimit - personalAllowance) * 0.20 + (income - basicRateUpperLimit) * 0.40;
+    } else {
+        tax = (basicRateUpperLimit - personalAllowance) * 0.20 + (higherRateUpperLimit - basicRateUpperLimit) * 0.40 + (income - higherRateUpperLimit) * 0.45;
+    }
+
+    return tax;
+}
